cfa1d4d84573264a9ea66ffc60347a39d47e91a3
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-07-20 16:32:29 -0500

    completes tasklister lab

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        handleToDo(e.target.new_task_description.value)
+        form.reset()
+    })
+})
+
+
+function handleToDo(todo) {
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    p.textContent = `${todo} `
+    p.appendChild(btn)
+
+    document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e) {
+    e.target.parentNode.remove()
+}
